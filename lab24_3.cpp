f33df4378d0b1db5290892c1adaf86e0cd10f279
Thanakrit <thanakrit_la@cmu.ac.th>
2026-03-06 15:23:26 +0700

    lab24_3.cpp completed

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
 //Write function count() here.
+
+int count(int target_data[], int size){
+	set<int> nonduplicate;
+	for(int i = 0; i < size; i++){
+		nonduplicate.insert(target_data[i]);
+	}
+	return nonduplicate.size();
+}
+
+
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
